f311a9164a2e1411977c618614bff81bc412ad24
UMAMAGESHWARIK <umssmageshwari@gmail.com>
2025-08-25 21:18:33 +0530

    Updated Sample.java for automatic jenkins build trigger

--- Sample.java
+++ Sample.java
@@ -1,9 +1,7 @@
-class Sample {
-    
+public class Sample {
     public static void main(String[] args) {
-        
-        System.out.println("Hello World !!!");
-
+        System.out.println("Hello Jenkins and Git Integration!");
+        System.out.println("Version 2.0 - Updated Release");
+        System.out.println("Testing automatic build trigger!");
     }
-
 }
