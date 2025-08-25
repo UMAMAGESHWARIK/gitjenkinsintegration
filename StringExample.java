e810611b3826871086cbb8969877365500d63f19
UMAMAGESHWARIK <umssmageshwari@gmail.com>
2025-08-25 13:02:52 +0530

    Initial commit

---
+++ StringExample.java
@@ -0,0 +1,12 @@
+class StringExample {
+
+    public static void main(String[] args) {
+        
+        String firstName = "Uma";
+        String lastName = "Mageshwari";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println("Full Name: " + fullName);
+    }
+}
